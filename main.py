0eb60e310e9274a59cae4cf8972151800f995906
sbm5761 <50812423+sbm5761@users.noreply.github.com>
2023-02-28 18:06:37 -0500

    Improved test_acc to 68%

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import os
 import gensim
 import numpy as np
 from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from pathlib import Path
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
 
 
 '''# GENERIC PLACEHOLDER UNTIL OUR MODEL TYPE IS DECIDED
@@ -17,8 +21,8 @@ data_dir = "data/train.txt"
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)'''
 
-# READ DATA FROM FILE INTO INPUT & LABELS
-# TODO: DECIDE HOW WE WILL PROCESS NEW LINES
+
+#Read in words to train
 file = open("data/train.txt", "r")
 data = []
 labels = []
@@ -34,9 +38,24 @@ for line in file:
         sentences.append(s)
         s=[]
     else:
-        data.append(word[0])
+        data.append(word[0]) #make lower and experiment
         labels.append(word[1])
         s.append(word[0])
+
+#After testing, show results of predicted WE with actual
+def test_predicted_list(words, predicted, actual): #len= 832
+    compare_list=[]
+
+    for i in range(len(words)):
+        compare_list.append([words[i], predicted[i], actual[i]])
+
+
+    #Write to text file
+    with open(r'./output.txt', 'w') as fp:
+        for item in compare_list:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+
 
 #Create word embeddings using Word2Vec
 #Do we need normalization?? Lower case of words?
@@ -51,13 +70,22 @@ for line in file:
 
     Source: https://radimrehurek.com/gensim/models/word2vec.html 
 '''
-def word_embeddings(sentences):
-    model = gensim.models.Word2Vec(min_count = 5, window=3, vector_size=20, sg=0) #Can change parameters
-    model.build_vocab(sentences)
-    model.train(sentences, total_examples=model.corpus_count, epochs=10) 
+def word_embeddings():
+    path = Path('./vectors.kv')
 
-    word_vectors = model.wv
-    word_dict= word_vectors.key_to_index #all words and their index
+    if path.is_file() is False: #If we need to generate word embeddings
+        #Current implementation at 68% for testing
+        model = gensim.models.Word2Vec(min_count = 15, negative= 20, window=1, epochs= 15, vector_size=100, sg=0) #Can change parameters
+        model.build_vocab(sentences)
+        model.train(sentences, total_examples=model.corpus_count, epochs=15) 
+
+        word_vectors = model.wv
+        word_vectors.save('vectors.kv')
+        word_dict= word_vectors.key_to_index #all words and their index
+    
+    else: #Vector file already exists
+        word_vectors = KeyedVectors.load('vectors.kv')
+        word_dict= word_vectors.key_to_index
 
     for keys in word_dict:
         if keys.isalnum(): #disregard punctuation
@@ -68,29 +96,40 @@ def word_embeddings(sentences):
             word_dict[keys]= vector_POS 
 
     #word_vectors[word]: gives vector for word
-
     return word_dict
 
 def Logistic_Regression(data):
-    #Separate data
+    ## Specific to Word2Vec Embeddings 
     values= list(data.values())
+    words_before= list(data.keys())
     word_embs=[]
     labels=[]
+    words=[]
     
-    for v in values:
-        if isinstance(v, list):
-            word_embs.append(list(v[0]))
-            labels.append(v[1])
+    for v in range(len(values)):
+        if isinstance(values[v], list):
+            word_embs.append(list(values[v][0]))
+            labels.append(values[v][1])
+            words.append(words_before[v])
+    #Until here, can comment out for other types of embeddings
+
+    #Split data
+    split = int(len(word_embs) * .15)
+    X_train = word_embs[split:]
+    y_train = labels[split:]
+    X_test = word_embs[:split]
+    y_test = labels[:split]
+    word_test = words[:split]
 
     #Build the model
-    class_model = LogisticRegression()
-    class_model.fit(word_embs, labels)
+    class_model = LogisticRegression(max_iter=300)
+    class_model.fit(X_train, y_train)
 
-    #Testing example on word "Pound"
-    x_test= data["pound"][0]
-    y_test= data["pound"][1]
-    y_pred = class_model.predict(x_test.reshape(1,-1))
+    #Testing
+    y_pred = class_model.predict(X_test)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
 
-word_embed_dict= word_embeddings(sentences) #Key= word, Value= [Vector_embeddings, POS-Tag]
-#X_train, X_test = train_test_split(word_embed, test_size=0.2)
+    #test_predicted_list(word_test, y_pred, y_test)
+
+word_embed_dict= word_embeddings() #Key= word, Value= [Vector_embeddings, POS-Tag]
 Logistic_Regression(word_embed_dict)
